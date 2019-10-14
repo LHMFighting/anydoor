22248da17a11f37dee961feff52065d80f934a0f
LHMFighting <2585283373@qq.com>
2019-10-14 23:02:08 +0800

    增加模板文件

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,20 +1,40 @@
 const fs = require('fs')
+const path = require('path')
+const Handlebars = require('handlebars')
 const promisify = require('util').promisify
 const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
+const mime = require('./mime')
+
+const tplPath = path.join(__dirname, '../template/dir.tpl')
+const source = fs.readFileSync(tplPath, 'utf-8')
+const template = Handlebars.compile(source)
+const config = require('../config/defaultConf')
 
 module.exports = async function (req, res, filePath) {
     try {
         const stats = await stat(filePath)
         if(stats.isFile()) {
+            const contentType = mime(filePath)
             res.statusCode = 200
-            res.setHeader('Content-Type', 'text/plain')
+            res.setHeader('Content-Type', contentType)
             fs.createReadStream(filePath).pipe(res)
         } else if (stats.isDirectory()) {
             const files = await readdir(filePath)
             res.statusCode = 200
-            res.setHeader('Content-Type', 'text/plain')
-            res.end(files.join(','))
+            res.setHeader('Content-Type', 'text/html')
+            const dir = path.relative(config.root, filePath)
+            const data = {
+                title: path.basename(filePath),
+                dir: dir ? `/${dir}` : '',
+                files: files.map(file => {
+                    return {
+                        file,
+                        icon: mime(file)
+                    }
+                })
+            }
+            res.end(template(data))
         }
     } catch (ex) {
         console.error(ex)
