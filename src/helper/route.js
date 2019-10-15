a46ead3ae9a0b5e55363fe2d96e62fc6bf7857c7
LHMFighting <2585283373@qq.com>
2019-10-15 23:01:36 +0800

    压缩、范围、缓存

--- src/helper/route.js
+++ src/helper/route.js
@@ -5,6 +5,9 @@ const promisify = require('util').promisify
 const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
 const mime = require('./mime')
+const compress = require('./compress')
+const range = require('./range')
+const isFresh = require('./cache')
 
 const tplPath = path.join(__dirname, '../template/dir.tpl')
 const source = fs.readFileSync(tplPath, 'utf-8')
@@ -16,9 +19,27 @@ module.exports = async function (req, res, filePath) {
         const stats = await stat(filePath)
         if(stats.isFile()) {
             const contentType = mime(filePath)
-            res.statusCode = 200
             res.setHeader('Content-Type', contentType)
-            fs.createReadStream(filePath).pipe(res)
+
+            if (isFresh(stats, req, res)) {
+                res.statusCode = 304
+                res.end()
+                return
+            }
+
+            let rs
+            const { code, start, end } = range(stats.size, req, res)
+            if (code === 200) {
+                res.statusCode = 200
+                rs = fs.createReadStream(filePath)
+            } else {
+                res.statusCode = 206
+                rs = fs.createReadStream(filePath, {start, end})
+            }
+            if (filePath.match(config.compress)) {
+                rs = compress(rs, req, res)
+            }
+            rs.pipe(res)
         } else if (stats.isDirectory()) {
             const files = await readdir(filePath)
             res.statusCode = 200
